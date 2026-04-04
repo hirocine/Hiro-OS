5d1c051e2bf2c62f3191994b49d07c65307b01a2
hirocine <gabriel@hiro.film>
2026-04-04 20:27:27 -0300

    Update index.ts

--- src/features/proposals/components/index.ts
+++ src/features/proposals/components/index.ts
@@ -1,3 +1,4 @@
 export { ProposalWizard } from './ProposalWizard';
+export { ProposalGuidedWizard } from './ProposalGuidedWizard';
 export { ProposalCard } from './ProposalCard';
 export { ProposalPublicPage } from './ProposalPublicPage';
